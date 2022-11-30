c56e4328d88eff6945230d799601517701e410d6
doocubii <doocubii@gmail.com>
2022-11-30 17:08:53 +0900

    설정을 분리

--- src/main/java/org/elasticsearch/index/analysis/BaikalNlpCaller.java
+++ src/main/java/org/elasticsearch/index/analysis/BaikalNlpCaller.java
@@ -17,7 +17,7 @@ import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-// import java.util.logging.Level;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Properties;
 
@@ -25,22 +25,36 @@ public class BaikalNlpCaller {
     LanguageServiceGrpc.LanguageServiceBlockingStub client;
     private final static Logger LOGGER = Logger.getGlobal();
     private List<String> stopTokens; // tokenMorphemes, 
-    final String DEF_STOP_TOKENS =  "E,IC,J,MAG,MAJ,MM,SP,SSC,SSO,SC,SE,XPN,XSA,XSN,XSV,UNA,NA,VSV";
-    final int DEF_PORT = 5656;
-    final String DEF_ADDRESS = "localhost";
+    final static String DEF_STOP_TOKENS =  "E,IC,J,MAG,MAJ,MM,SP,SSC,SSO,SC,SE,XPN,XSA,XSN,XSV,UNA,NA,VSV";
+    final static int DEF_PORT = 5656;
+    final static String DEF_ADDRESS = "localhost";
     /*
     Boolean isTest = false;
     String configPath;
     */
-    Boolean isTest = true;
+    final static Boolean isTest = true;
     String configPath = "";
     String ip;
     int port;
     ManagedChannel channel;
-    final String CONFIG_FILE =  "/usr/share/elasticsearch/data/config.properties";
+    final static String CONFIG_FILE =  "/usr/share/elasticsearch/data/config.properties";
 
-    public BaikalNlpCaller() {
-        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+    public static class NlpSettings {
+        public String ip;
+        public int port;
+        public ArrayList<String> stopTokens;
+        public NlpSettings(String ip, int port, ArrayList<String> stopTokens ) {
+            this.ip = ip;
+            this.port = port;
+            this.stopTokens = stopTokens;
+        }
+    }
+
+    public static NlpSettings getSettingsFromConfig() {
+        return getSettingsFromConfig("");
+    }
+    public static NlpSettings getSettingsFromConfig(String configPath) {
+        return AccessController.doPrivileged((PrivilegedAction<NlpSettings>) () -> {
             try {
                 if (!isTest) {
                     String pathToRead = configPath;
@@ -52,39 +66,48 @@ public class BaikalNlpCaller {
                 
                     Properties p = new Properties();			
                     p.load(file); // 파일 열어줌
-                    ip = p.getProperty("NLP_server_address" , DEF_ADDRESS); 
-                    port = Integer.parseInt(p.getProperty("NLP_server_port", String.valueOf(DEF_PORT) )); 
+                    String ip = p.getProperty("NLP_server_address" , DEF_ADDRESS); 
+                    int port = Integer.parseInt(p.getProperty("NLP_server_port", String.valueOf(DEF_PORT) )); 
                     // String strs = p.getProperty("token_morphemes",  "NNP,NNG,NNB,NF,VV,SL,SH,SN"); 
                     // tokenMorphemes = new ArrayList<String>(Arrays.asList(strs.split(",")));
-
+        
                     String strs = p.getProperty("stop_tokens",  DEF_STOP_TOKENS); 
-                    stopTokens = new ArrayList<String>(Arrays.asList(strs.split(",")));
-
+                    ArrayList<String> stopTokens = new ArrayList<String>(Arrays.asList(strs.split(",")));
+                    return new NlpSettings(ip, port, stopTokens);
                    
-
-                } else {                    
-                    ip = DEF_ADDRESS; 
-                    port = DEF_PORT;
-                    stopTokens = new ArrayList<String>(Arrays.asList(DEF_STOP_TOKENS.split(",")));
-                }
+        
+                } else {
+                    return new NlpSettings(DEF_ADDRESS, DEF_PORT, new ArrayList<String>(Arrays.asList(DEF_STOP_TOKENS.split(","))));                    
+                }                
             } catch (Exception e) {
                 LOGGER.warning(e.getMessage());
                 return null;
             }
-            //LOGGER.setLevel(Level.INFO);
-            //LOGGER.info("Client is ok");
-            return null;
+
         });
+        
+    }
+
+    public BaikalNlpCaller() {
+        this(getSettingsFromConfig());
+    }
+
+    public BaikalNlpCaller(NlpSettings settings) {
+        ip = settings.ip;
+        port = settings.port;
+        stopTokens = settings.stopTokens;
+        LOGGER.info(String.format("NLP SERVER - %s:%d", ip, port));
     }
 
     public AnalyzeSyntaxResponse send(String text) {
+        
         return AccessController.doPrivileged((PrivilegedAction<AnalyzeSyntaxResponse>) () -> {
             AnalyzeSyntaxResponse response = null;
             try {
                 channel = ManagedChannelBuilder.forAddress(ip, port).usePlaintext().build();
                 client = LanguageServiceGrpc.newBlockingStub(channel);
-                //LOGGER.setLevel(Level.INFO);
-                //LOGGER.info(text);
+                LOGGER.setLevel(Level.INFO);
+                LOGGER.info("analyze - '"+text+"'");
                 Document document = Document.newBuilder().setContent(text).setLanguage("ko-KR").build();
                 AnalyzeSyntaxRequest request = AnalyzeSyntaxRequest.newBuilder().setDocument(document).build();
                 response = client.analyzeSyntax(request);
